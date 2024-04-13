c7188b9d34cc49e4d89de0bce9f978d3c2196376
akhalim1 <156042742+akhalim1@users.noreply.github.com>
2024-04-13 16:29:02 -0700

    adding chase player action

--- Assets/Enemies/EnemyBrain.cs
+++ Assets/Enemies/EnemyBrain.cs
@@ -7,7 +7,7 @@ public class EnemyBrain: MonoBehaviour
     [SerializeField] private string initState;
     [SerializeField] private FSMState[] states;
     public FSMState CurrentState { get; set; }
-
+    public Transform Player { get; set; }
     private void Start()
     {
         ChangeState(initState);
---
+++ Assets/Scripts/Waypoint/Waypoint.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] private Vector3[] points;
+}
